a43d3115b87e8724602f7f1c74383267b46508e5
Mau8877 <maurogupri@gmail.com>
2026-08-08 13:39:01 -0400

    feat[BACK]: Inicializacion Backend

---
+++ backend/frisoft/src/main/java/com/pruebafrisoft/frisoft/FrisoftApplication.java
@@ -0,0 +1,13 @@
+package com.pruebafrisoft.frisoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrisoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrisoftApplication.class, args);
+	}
+
+}
---
+++ backend/frisoft/src/test/java/com/pruebafrisoft/frisoft/FrisoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruebafrisoft.frisoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrisoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
